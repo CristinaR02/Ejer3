15ab000c874a448ebd7b072e3368fde579eccc4c
Nullgosling <Nullgosling@192.168.1.55>
2021-10-26 13:39:01 +0200

    Comentario

--- ExamEjer3/src/Ejer3/HolaMundo.java
+++ ExamEjer3/src/Ejer3/HolaMundo.java
@@ -2,13 +2,9 @@ package Ejer3;
 
 public class HolaMundo {
 
-	public HolaMundo() {
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Hola Mundo");
 	}
 
 }
